0207061a62b40e29de1e4bce20b8425b775ab097
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-01-15 19:15:51 +1100

    create_teaching_dir

--- R/create.R
+++ R/create.R
@@ -4,48 +4,74 @@
 #'
 #' @param code the code of the course
 #' @param name the short name of the course. There should be no spaces.
+#' @param proj_name the project name for Rpro
 #' @param full_name the full name of the course e.g. Wild-Caught Data
 #' @param destdir the directory path
-#' @param layout a function that creates the directory layout.
-#' See `monash::layout_teaching_dir` for an example.
+#' @param layout the yaml file name (TODO: or a list)
 #' @param rstudio launch in rstudio?
 #' @param open open after creating teaching directory?
 #'
 #' @name working-dir
 NULL
 
+eval_inline <- function(text) {
+  exprs <- stringr::str_match_all(text, "`r ([^`]+)`")[[1]][, 2]
+  out <- unname(sapply(exprs, function(aexpr) eval(parse(text = aexpr))))
+  text_new <- text
+  for(a in out) {
+    text_new <- stringr::str_replace(text_new, "`r [^`]+`", as.character(a))
+  }
+  text_new
+}
+
+create_folder_structure <- function(layout, parent = "./") {
+  for(afolder in names(layout)) {
+    if(afolder==".files") {
+      for(afile in layout$.files) {
+        file_copy(eval_inline(afile), parent)
+      }
+    } else {
+      create_directory(fs::path(parent, afolder))
+      create_folder_structure(layout[[afolder]], parent = fs::path(parent, afolder))
+    }
+  }
+}
+
 
 #' @describeIn working-dir Create a directory structure specified by `layout`.
 #' @export
 create_bare_dir <- function(code,
                             name = code,
+                            proj_name = paste0(name, "-private"),
                             full_name = name,
                             destdir = code,
-                            layout = function() {},
+                            layout = NULL,
                             rstudio = rstudioapi::isAvailable(),
                             open = interactive()) {
 
-  destdir <- user_path_prep(destdir)
+  layout <- layout %||% system.file("templates", "_structure.yml", package = "monash")
+  folder <- yaml::read_yaml(layout)$folder
   check_path_is_directory(path_dir(destdir))
 
-  create_directory(destdir)
-  old_project <- proj_set(destdir, force = TRUE)
-  on.exit(proj_set(old_project), add = TRUE)
+  projdir <- fs::path(destdir, proj_name)
+  create_directory(projdir)
+  old_project <- usethis::proj_set(projdir, force = TRUE)
+  on.exit(usethis::proj_set(old_project), add = TRUE)
 
-  layout()
+  create_folder_structure(folder, projdir)
 
   if (rstudio) {
     use_rstudio()
   }
 
   if (open) {
-    if (proj_activate(path)) {
+    if (usethis::proj_activate(projdir)) {
       # Working directory/active project changed; so don't undo on exit
       on.exit()
     }
   }
 
-  invisible(proj_get())
+  invisible(usethis::proj_get())
 
 }
 
@@ -66,7 +92,7 @@ create_workshop_dir <- function(name, full_name = name,
 create_teaching_dir <- function(code, name = code, full_name = name,
                                 destdir = getOption("monash.teaching_dir"),
                                 rstudio = TRUE, open = TRUE,
-                                layout = layout_teaching_dir) {
+                                layout = NULL) {
   destdir <- destdir %||% getwd()
   create_bare_dir(name = name, full_name = full_name, destdir = destdir,
                   rstudio = rstudio, layout = layout, open = open)
